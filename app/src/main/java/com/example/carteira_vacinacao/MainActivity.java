8f44224e24f84a7de4619a9fa7df29aeabf19ddf
Jéssica Cardoso <jessica.cardosor@gmail.com>
2021-06-09 11:37:11 -0300

    Inclusão do edittext

--- app/src/main/java/com/example/carteira_vacinacao/MainActivity.java
+++ app/src/main/java/com/example/carteira_vacinacao/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.carteira_vacinacao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText et = (EditText) findViewById(R.id.txtSub);
+        String name = et.getText().toString();
     }
 }
