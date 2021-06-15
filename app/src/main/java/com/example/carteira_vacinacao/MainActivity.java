d216bc7e79d3ef9802672ee7e94a2b160fdee0f3
Jéssica Cardoso <jessica.cardosor@gmail.com>
2021-06-15 20:36:16 -0300

    Ajustes tela cadastro e criacao da class carteira

--- app/src/main/java/com/example/carteira_vacinacao/MainActivity.java
+++ app/src/main/java/com/example/carteira_vacinacao/MainActivity.java
@@ -2,16 +2,36 @@ package com.example.carteira_vacinacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.minha_carteira);
+        setContentView(R.layout.activity_main);
         //EditText et = (EditText) findViewById(R.id.txtSub);
         //String name = et.getText().toString();
+
+        button = (Button) findViewById(R.id.login);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                criarActivityLogin();
+            }
+        });
+
     }
+    public void criarActivityLogin(){
+        Intent intent = new Intent(this, carteira.class);
+        startActivity(intent);
+    }
+    /*private void criarActivityCadastro(){}*/
 }
