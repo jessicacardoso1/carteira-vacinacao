0f033884c3e9ada3268c297be48b170a17214ade
LudmilaBGomes <lubrito123@gmail.com>
2021-06-14 23:50:45 -0300

    Adição de emblemas nos cards da vacina

--- app/src/main/java/com/example/carteira_vacinacao/MainActivity.java
+++ app/src/main/java/com/example/carteira_vacinacao/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detalhes_vacina);
+        setContentView(R.layout.minha_carteira);
         //EditText et = (EditText) findViewById(R.id.txtSub);
         //String name = et.getText().toString();
     }
