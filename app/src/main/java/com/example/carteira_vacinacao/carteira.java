0f9baf1739e8ee9e9bcf2874d5de591db54740e2
Jéssica Cardoso <jessica.cardosor@gmail.com>
2021-06-15 21:10:36 -0300

    Criando a class detalhes vacina e inserindo o link

--- app/src/main/java/com/example/carteira_vacinacao/carteira.java
+++ app/src/main/java/com/example/carteira_vacinacao/carteira.java
@@ -1,14 +1,32 @@
 package com.example.carteira_vacinacao;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class carteira extends AppCompatActivity {
+    private Button button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.minha_carteira);
+
+        button = (Button) findViewById(R.id.nomeVacina);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                criarActivityDetalhesVacina();
+            }
+        });
+    }
+
+    public void criarActivityDetalhesVacina(){
+        Intent intent = new Intent(this, detalhes_vacina.class);
+        startActivity(intent);
     }
 }
